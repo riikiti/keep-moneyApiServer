0cbcdf2d186ded07a1b1d586c8c4cd9dfbd101bf
Ruslan <rik.chanel@gmail.com>
2023-03-31 16:35:53 +0300

    add migrations for shops, categories and IncomeCategories

---
+++ app/Models/IncomeCategories.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IncomeCategories extends Model
+{
+    use HasFactory;
+}
