62564bbfb15f1708f3697a44fbbac042c6405bea
Ruslan <rik.chanel@gmail.com>
2023-05-07 17:04:43 +0300

    create form for create budget

--- resources/js/components/SwiperCards.vue
+++ resources/js/components/SwiperCards.vue
@@ -1,13 +1,63 @@
 <template>
+    <button class="button" @click="modalCreate()">Создать карту</button>
+    <teleport to=".modals" v-if="modalForCreate">
+        <Modal :status="modalForCreate" @modalClose="modalCreate()">
+            <template v-slot:modalContent>
+                <form class="form" @submit.prevent="modalOpen()">
+                    <h2 class="title title--2">Создание карты</h2>
+                    <div class="form__block">
+                        <label class="title title--3">Банк</label>
+                        <bank-selector :option="banks"
+                                       @getSelect="getSelect"
+                        ></bank-selector>
+                    </div>
+
+                    <div class="form__block">
+                        <label class="title title--3">Тип карты</label>
+                        <div class="form__block-radio">
+                            <div class="form__block-radio__wrap">
+                                <input type="radio" value="mir" v-model="createData.type">
+                                <label>Mir</label>
+                            </div>
+                            <div class="form__block-radio__wrap">
+                                <input type="radio" value="visa" v-model="createData.type">
+                                <label>Visa</label>
+                            </div>
+                            <div class="form__block-radio__wrap">
+                                <input type="radio" value="mc" v-model="createData.type">
+                                <label>MasterCard</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form__block">
+                        <label class="title title--3">Последние 4 цифры</label>
+                        <input type="number" v-model="createData.numbers" required/>
+                    </div>
+                    <div class="form__block">
+                        <label class="title title--3">Дата окончания карты</label>
+                        <input type="text" v-model="createData.last_date" required/>
+                    </div>
+                    <div class="form__block">
+                        <label class="title title--3">Количсетво денег на карте</label>
+                        <input type="number" v-model="createData.budget" required/>
+                    </div>
+                    <button class="form__btn" @click="posthData(createData)">
+                        Создать
+                    </button>
+                </form>
+            </template>
+        </Modal>
+    </teleport>
     <swiper :slides-per-view="1" :modules="modules" :pagination="true" :grab-cursor="true" class="swiper" v-if="data">
-        <swiper-slide v-for="(item, index) in data" :key="item.id">
+        <swiper-slide v-for="item in data" :key="item.id">
             <div class="bank-card__wrap">
                 <div class="bank-card__content" :style="{ color: item.bank.text_color , background: item.bank.color }">
                     <img :src="cardBank(item.bank.name)" :alt="item.bank.name" class="bank-card__bank">
                     <div class="bank-card__action">
-                        <img src="../assets/img/svg/pen.svg" alt="pen">
                         <button @click="modalOpenPlus(item)">+</button>
                         <button @click="modalOpenMinus(item)">-</button>
+                        <img src="../assets/img/svg/pen.svg" alt="pen">
+                        <img src="../assets/img/svg/trash.svg" alt="trash" @click="deleteData(item.id)">
                     </div>
                     <div class="bank-card__numbers"><span>**** **** **** {{ item.numbers }}</span></div>
                     <div class="bank-card__date">
@@ -70,16 +120,29 @@ import 'swiper/css/pagination';
 import axios from "axios";
 import {onMounted, ref} from "vue";
 import Modal from "../components/Modal.vue";
+import BankSelector from "../components/CategoriesSelector.vue";
 
 
 const modules = [Pagination, Navigation, Scrollbar, A11y];
 const modalPlus = ref(false);
 const modalMinus = ref(false);
+const modalForCreate = ref(false);
 const modalItem = ref(null);
 const data = ref(null);
 const type = ref(null);
 const id = localStorage.getItem('id');
 const dataForUpdate = ref({});
+const createData = ref([]);
+const selectCategories = ref({});
+const banks = ref(null)
+
+
+const getSelect = (item) => {
+    console.log(item.id)
+    selectCategories.id = item.id
+    selectCategories.name = item.name
+}
+
 
 const fetchData = async () => {
     axios
@@ -117,6 +180,12 @@ const cardType = (item) => {
             return 'http://127.0.0.1:5173/resources/js//assets/img/DebitCards/MC.png'
     }
 }
+
+
+const modalCreate = () => {
+    modalForCreate.value = !modalForCreate.value;
+    console.log(modalForCreate.value);
+};
 
 
 const modalOpenPlus = (index) => {
@@ -171,6 +240,33 @@ const updateData = async (item) => {
             console.log(error);
         });
 };
+
+const deleteData = async (id) => {
+    axios
+        .delete(`http://127.0.0.1:8000/api/v1/income/${id}`)
+        .then((response) => {
+            console.log(response.data);
+            fetchData();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+const fetchCategories = async () => {
+    axios
+        .get('http://127.0.0.1:8000/api/v1/bank')
+        .then((response) => {
+            console.log(response.data)
+            banks.value = response.data.data;
+            console.log(banks.value)
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+fetchCategories()
 
 
 onMounted(fetchData)
