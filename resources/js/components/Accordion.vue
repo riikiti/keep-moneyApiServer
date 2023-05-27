f7844bdc2701f718c6e9064f520e071f59332ee6
Ruslan <rik.chanel@gmail.com>
2023-05-27 16:49:29 +0300

    make accordions

---
+++ resources/js/components/Accordion.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="accordion" @click="openAccordion()" :class="{ 'accordion__active': open }">
+        <div class="accordion__title">
+            <h2 class="title title--3">
+                <slot name="title">
+
+                </slot>
+            </h2>
+            <div class="accordion__title-close">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        <div class="accordion__content">
+            <p class="title title--4">
+                <slot name="content">
+
+                </slot>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const open = ref(false);
+
+
+const openAccordion = () => {
+    open.value = !open.value;
+}
+
+</script>
+
+
--- resources/js/views/ProfileQuestionView.vue
+++ resources/js/views/ProfileQuestionView.vue
@@ -1,15 +1,55 @@
 <template>
 
     <div class="profile" :class="{ 'profile-active': menu}">
-        <h1 class="title title--2">Часто задаваемые вопросы?</h1>
-        здесть будет аккордион с вопросами
+        <div class="profile__header-question">
+            <h1 class="title title--2">Часто задаваемые вопросы?</h1>
+
+        </div>
         <ProfileAside @openMenu="openMenu()"></ProfileAside>
+        <div class="profile__question">
+            <div class="profile__question-subheader">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda commodi dicta eaque
+                    error
+                    fugit id incidunt iure laborum modi natus nobis, numquam officia praesentium quam, quod sit, unde
+                    voluptas.</p>
+            </div>
+            <div class="accordions__wrap">
+                <accordion>
+                    <template v-slot:title>
+                        Общий график
+                    </template>
+                    <template v-slot:content>
+                        Общий график
+                    </template>
+                </accordion>
+                <accordion>
+                    <template v-slot:title>
+                        Общий график
+                    </template>
+                    <template v-slot:content>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam, at autem ex
+                        explicabo
+                        fugit hic ipsam quisquam recusandae sint, temporibus unde? Dolores libero minus, nostrum officia
+                        pariatur suscipit voluptas.
+                    </template>
+                </accordion>
+                <accordion>
+                    <template v-slot:title>
+                        Общий график
+                    </template>
+                    <template v-slot:content>
+                        Общий график
+                    </template>
+                </accordion>
+            </div>
+        </div>
     </div>
 </template>
 
 
 <script setup>
 import ProfileAside from "../components/PortfolioAside.vue";
+import Accordion from "../components/Accordion.vue";
 import {ref} from "vue";
 
 const menu = ref(ref(localStorage.getItem("is_expanded") === "true"));
