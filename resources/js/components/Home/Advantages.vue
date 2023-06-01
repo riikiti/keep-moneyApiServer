889992066bc84c8761d7ceb7362ea38fd2ea6d1e
Ruslan <rik.chanel@gmail.com>
2023-06-01 15:56:16 +0300

    start home page

---
+++ resources/js/components/Home/Advantages.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="advantages">
+        <div class="advantages__block">
+            <div class="advantages__block-img">
+                <img src="../../assets/img/homeImages/2.png" alt="man">
+            </div>
+            <div class="advantages__block-content">
+                <h2 class="title title--2 advantages__block-content__title">
+                    Lorem ipsum dolor sit amet
+                </h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae culpa cumque dolorem laborum
+                    officiis recusandae rerum soluta ut! Dolores eos et officia quia voluptates! Atque expedita neque
+                    quas sit ullam.</p>
+            </div>
+        </div>
+        <div class="advantages__block">
+            <div class="advantages__block-content">
+                <h2 class="title title--2 advantages__block-content__title">
+                    Lorem ipsum dolor sit amet
+                </h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae culpa cumque dolorem laborum
+                    officiis recusandae rerum soluta ut! Dolores eos et officia quia voluptates! Atque expedita neque
+                    quas sit ullam.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae culpa cumque dolorem laborum
+                    officiis recusandae rerum soluta ut! Dolores eos et officia quia voluptates! Atque expedita neque
+                    quas sit ullam.</p>
+            </div>
+            <div class="advantages__block-img">
+                <img src="../../assets/img/homeImages/9.png" alt="man">
+            </div>
+        </div>
+        <div class="advantages__block">
+            <div class="advantages__block-img">
+                <img src="../../assets/img/homeImages/3.png" alt="man">
+            </div>
+            <div class="advantages__block-content">
+                <h2 class="title title--2 advantages__block-content__title">
+                    Lorem ipsum dolor sit amet
+                </h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae culpa cumque dolorem laborum
+                    officiis recusandae rerum soluta ut! Dolores eos et officia quia voluptates! Atque expedita neque
+                    quas sit ullam.</p>
+            </div>
+        </div>
+        <div class="advantages__block">
+            <div class="advantages__block-content">
+                <h2 class="title title--2 advantages__block-content__title">
+                    Lorem ipsum dolor sit amet
+                </h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae culpa cumque dolorem laborum
+                    officiis recusandae rerum soluta ut! Dolores eos et officia quia voluptates! Atque expedita neque
+                    quas sit ullam.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae culpa cumque dolorem laborum
+                    officiis recusandae rerum soluta ut! Dolores eos et officia quia voluptates! Atque expedita neque
+                    quas sit ullam.</p>
+            </div>
+            <div class="advantages__block-img">
+                <img src="../../assets/img/homeImages/5.png" alt="man">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+</script>
+
+
--- resources/js/views/HomeView.vue
+++ resources/js/views/HomeView.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="home">
-    <HeaderHome></HeaderHome>
-    <nav style="margin-top: 100px">
-    </nav>
-  </div>
+    <div class="home">
+        <HeaderHome></HeaderHome>
+        <advantages></advantages>
+    </div>
 </template>
 
 <script setup>
 
 import HeaderHome from "../components/HeaderHome.vue";
+import advantages from "../components/Home/Advantages.vue";
+import Advantages from "@/components/Home/Advantages.vue";
 
 </script>
 
